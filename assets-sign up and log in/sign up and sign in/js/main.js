729debe23db67557899c6d7d62f322248397e5f6
agani-ux <ivysongwa@gmail.com>
2024-05-23 22:00:01 +0300

    name edited out

--- assets-sign up and log in/sign up and sign in/js/main.js
+++ assets-sign up and log in/sign up and sign in/js/main.js
@@ -1,3 +1,50 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6pVqxO00QpZ3iW9XUDpmo1OiBObCh2YY",
+  authDomain: "lizer-bookings.firebaseapp.com",
+  projectId: "lizer-bookings",
+  storageBucket: "lizer-bookings.appspot.com",
+  messagingSenderId: "785741031288",
+  appId: "1:785741031288:web:d901cfce9081d0f2844a8d",
+  measurementId: "G-TXYKHZZ9XX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//inputs
+const nameInput = document.getElementById('name');
+
+
+//submit button
+const submit = document.getElementById('signup');
+submit.addEventListener("click",function(event){
+    
+    event.preventDefault()
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value; 
+    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        alert("Creating account...")
+        // ...
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage)
+    });
+})
+
 (function ($) {
     "use strict";
 
