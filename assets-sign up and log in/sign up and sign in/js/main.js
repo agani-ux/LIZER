c354ce76e174d6816e33b1cab4938679335e8f9b
agani-ux <ivysongwa@gmail.com>
2024-05-24 00:19:52 +0300

    sign up test 6

--- assets-sign up and log in/sign up and sign in/js/main.js
+++ assets-sign up and log in/sign up and sign in/js/main.js
@@ -127,12 +127,12 @@ submit.addEventListener("click",function(event){
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js"; // Correct import for auth
-
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA6pVqxO00QpZ3iW9XUDpmo1OiBObCh2YY",
   authDomain: "lizer-bookings.firebaseapp.com",
@@ -146,27 +146,30 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app); // Initialize the auth instance
 
-// Submit button
+//inputs
+const nameInput = document.getElementById('name');
+
+
+//submit button
 const submit = document.getElementById('signup');
-submit.addEventListener("click", function(event) {
-  event.preventDefault();
+submit.addEventListener("click",function(event){
+    
+    event.preventDefault()
 
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('pass').value;
-
-  signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    alert("Creating account...");
-    // ...
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    alert(errorMessage);
-  });
-});
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value; 
+    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        alert("Creating account...")
+        // ...
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage)
+    });
+})
 
 (function ($) {
   "use strict";
