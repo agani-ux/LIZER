39ae3d6ece9894b840662d03eacf88034b80e9eb
agani-ux <ivysongwa@gmail.com>
2024-05-24 08:58:22 +0300

    booking test 7

--- Admin dashboard/assets-booking form/js/main.js
+++ Admin dashboard/assets-booking form/js/main.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
